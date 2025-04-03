dd377a35f1fd7c453f0cb1524a1a1502443d469b
Théo Poizat <hello@zatteo.com>
2025-04-03 10:18:56 +0200

    feat: Upgrade packages

--- src/components/AppProviders.jsx
+++ src/components/AppProviders.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { BarProvider } from 'cozy-bar'
 import { CozyProvider } from 'cozy-client'
+import { DataProxyProvider } from 'cozy-dataproxy-lib'
 import { WebviewIntentProvider } from 'cozy-intent'
 import { BreakpointsProvider } from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { I18n } from 'cozy-ui/transpiled/react/providers/I18n'
@@ -10,11 +11,13 @@ const AppProviders = ({ client, lang, polyglot, children }) => {
   return (
     <WebviewIntentProvider>
       <CozyProvider client={client}>
-        <BarProvider>
-          <I18n lang={lang} polyglot={polyglot}>
-            <BreakpointsProvider>{children}</BreakpointsProvider>
-          </I18n>
-        </BarProvider>
+        <DataProxyProvider>
+          <BarProvider>
+            <I18n lang={lang} polyglot={polyglot}>
+              <BreakpointsProvider>{children}</BreakpointsProvider>
+            </I18n>
+          </BarProvider>
+        </DataProxyProvider>
       </CozyProvider>
     </WebviewIntentProvider>
   )
