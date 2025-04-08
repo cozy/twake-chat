23fc37d75e5c52acb71f01660c89a22829413d51
Théo Poizat <hello@zatteo.com>
2025-04-08 15:40:00 +0200

    feat: Remove bar title in mobile

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,25 +1,14 @@
 import React from 'react'
 
-import { BarComponent, BarCenter } from 'cozy-bar'
-import { useClient } from 'cozy-client'
+import { BarComponent } from 'cozy-bar'
 import { useExternalBridge } from 'cozy-external-bridge/container'
-import Typography from 'cozy-ui/transpiled/react/Typography'
-import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 
 const AppLayout = () => {
-  const { isMobile } = useBreakpoints()
-  const client = useClient()
-
   useExternalBridge('https://chat.twake.app')
 
   return (
     <>
       <BarComponent />
-      {isMobile && (
-        <BarCenter>
-          <Typography variant="h5">{client.appMetadata.slug}</Typography>
-        </BarCenter>
-      )}
       <iframe id="embeddedApp" src="https://chat.twake.app"></iframe>
     </>
   )
