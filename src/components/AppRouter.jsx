0491abeab7365e517b35a9afc069272eb6909df2
Théo Poizat <hello@zatteo.com>
2025-04-16 15:02:05 +0200

    feat: Enable search and AI

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import { BarRoutes } from 'cozy-bar'
+
 import AppLayout from '@/components/AppLayout'
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {BarRoutes.map(BarRoute => BarRoute)}
         <Route path="*" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
