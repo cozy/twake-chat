2704c106bfb659a1a2983a8693c0fae1adbf2a97
Théo Poizat <hello@zatteo.com>
2025-05-22 14:39:32 +0200

    fix: Redirect on load display blank page
    With updated version of cozy-external-bridge, useExternalBridge does not redirect the iframe anymore and instead return the correct url to load.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,16 @@ import { useExternalBridge } from 'cozy-external-bridge/container'
 import flag from 'cozy-flags'
 
 const App = () => {
-  const embeddedChatUrl = flag('chat.embedded-app-url')
+  const embeddedAppRootUrl = flag('chat.embedded-app-url')
 
-  const { isReady } = useExternalBridge(embeddedChatUrl)
+  const { isReady, urlToLoad } = useExternalBridge(embeddedAppRootUrl)
 
   // We can not return null if bridge is not ready because to setup
   // the bridge we need iframe HTML element
   return (
     <iframe
       id="embeddedApp"
-      src={isReady ? embeddedChatUrl : null}
+      src={isReady ? urlToLoad : null}
       allow="clipboard-read; clipboard-write"
     ></iframe>
   )
